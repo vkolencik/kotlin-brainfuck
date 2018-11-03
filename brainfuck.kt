451c284562957de4001e75d39538e9eca62644d0
vkolencik <kolencik@gmail.com>
2018-11-03 08:05:36 +0100

    Formatting, naming

--- brainfuck.kt
+++ brainfuck.kt
@@ -12,12 +12,10 @@ class BrainLuck(private val code: String) {
     get() = memory[dataPtr] ?: 0
     set(value: Byte) { memory[dataPtr] = value }
   
-  
-	
   fun process(input: String): String {
     var inputPtr = 0
     var output = StringBuilder()
-    var parens = processParens(code)
+    var matchingParens = compileParens(code)
     
     loop@ while (codePtr in code.indices) {
       when (code[codePtr]) {
@@ -27,8 +25,8 @@ class BrainLuck(private val code: String) {
         '-' -> mem--
         '.' -> if (mem == 255.toByte()) break@loop else output.append(mem.toChar().toString())
         ',' -> mem = input[inputPtr++].toByte()
-        '[' -> if (mem == 0.toByte()) codePtr = parens[codePtr]!!
-        ']' -> if (mem != 0.toByte()) codePtr = parens[codePtr]!!
+        '[' -> if (mem == 0.toByte()) codePtr = matchingParens[codePtr]!!
+        ']' -> if (mem != 0.toByte()) codePtr = matchingParens[codePtr]!!
       }
       codePtr++      
     }
@@ -36,25 +34,24 @@ class BrainLuck(private val code: String) {
     return output.toString()
   }
   
-  fun processParens(code: String): Map<Int, Int> {
+  fun compileParens(code: String): Map<Int, Int> {
     val stack = Stack<Int>()
-    val parens = HashMap<Int, Int>()
+    val matchingParens = HashMap<Int, Int>()
     code.forEachIndexed { i, ch ->
       if (ch == '[')
         stack.push(i)
       if (ch == ']') {
         if (stack.empty())
           throw IllegalArgumentException("']' without a matching '[' on position $i")
-        parens[i] = stack.pop()
-        parens[parens[i]!!] = i // bidirectional map
+        matchingParens[i] = stack.pop()
+        matchingParens[matchingParens[i]!!] = i // bidirectional map
       }
     }
     
     if (!stack.empty())
       throw IllegalArgumentException("'[' without a matching ']' on position ${stack.pop()}")
     
-    return parens
+    return matchingParens
   }
-  
-  
+    
 }
